bfce953fdcd5c88e3c9135868752e0a66f885fae
lapinRagnar <lapinragnar@gmail.com>
2023-05-18 00:28:42 +0200

    basic window && widgets with ttk

--- 01_window_and_widgets.py
+++ 01_window_and_widgets.py
@@ -1,13 +1,31 @@
 import tkinter as tk
+from tkinter import ttk
+
+def button_func():
+  print('salut')
 
 # create window
 window = tk.Tk()
 window.title('window and widgets')
 window.geometry('800x500')
 
-# create widgets
+# widgets
+# ttk label
+label = ttk.Label(master=window, text='this is a test')
+label.pack()
+
+# tk text
 text = tk.Text(master=window)
 text.pack()
+
+# ttk entry
+entry = ttk.Entry(master=window)
+entry.pack()
+
+# ttk button
+button = ttk.Button(master=window, text='a button', command=button_func)
+button.pack()
+
 # run
 window.mainloop()
 
