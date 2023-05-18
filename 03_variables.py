8b463d8cc6df6f407ed593ab0ff1983496dc34ee
lapinRagnar <lapinragnar@gmail.com>
2023-05-18 10:17:17 +0200

    exercice

--- 03_variables.py
+++ 03_variables.py
@@ -22,6 +22,18 @@ entry.pack()
 button = ttk.Button(master=window, text='mon bouton', command=on_button_click)
 button.pack()
 
+# exercice
+exercice_var = tk.StringVar(value="change this text")
+
+mon_label_1 = ttk.Label(master=window, text='mon label 1', textvariable=exercice_var)
+mon_label_1.pack()
+
+mon_entry_1 = ttk.Entry(master=window, textvariable=exercice_var)
+mon_entry_1.pack()
+
+mon_entry_2 = ttk.Entry(master=window, textvariable=exercice_var)
+mon_entry_2.pack()
+
 # run
 window.mainloop()
 
