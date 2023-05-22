16731ecc1e320815fc0fbdaa65f29688a053530b
lapinRagnar <lapinragnar@gmail.com>
2023-05-22 23:50:46 +0200

    layout with grid()

--- 16_layout.py
+++ 16_layout.py
@@ -13,18 +13,18 @@ label1 = ttk.Label(window, text='mon label 1', background='purple')
 label2 = ttk.Label(window, text='mon label 2', background='green')
 
 # pack
-label1.pack(side='left', expand=True, fill='both')
-label2.pack(side='right', expand=True, fill='both')
+# label1.pack(side='left', expand=True, fill='both')
+# label2.pack(side='right', expand=True, fill='both')
 
 # grid
-# window.columnconfigure(0, weight=1)
-# window.columnconfigure(1, weight=1)
-# window.columnconfigure(2, weight=2)
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=1)
+window.columnconfigure(2, weight=2)
 
-# window.rowconfigure(0, weight=1)
+window.rowconfigure(0, weight=1)
 
-# label1.grid(row=0, column=1, sticky='nsew')
-# label2.grid(row=0, column=0, sticky='nsew')
+label1.grid(row=0, column=1, sticky='nsew')
+label2.grid(row=0, column=0, sticky='nsew')
 
 # run
 window.mainloop()
