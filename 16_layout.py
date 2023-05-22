376e6ade64527ac6c104cc1123a97adea527eff3
lapinRagnar <lapinragnar@gmail.com>
2023-05-23 00:07:10 +0200

    layout - with place()

--- 16_layout.py
+++ 16_layout.py
@@ -16,16 +16,24 @@ label2 = ttk.Label(window, text='mon label 2', background='green')
 # label1.pack(side='left', expand=True, fill='both')
 # label2.pack(side='right', expand=True, fill='both')
 
+# --------------------------------------------------------------------
+
 # grid
-window.columnconfigure(0, weight=1)
-window.columnconfigure(1, weight=1)
-window.columnconfigure(2, weight=2)
+# window.columnconfigure(0, weight=1)
+# window.columnconfigure(1, weight=1)
+# window.columnconfigure(2, weight=2)
 
-window.rowconfigure(0, weight=1)
-window.rowconfigure(1, weight=1)
+# window.rowconfigure(0, weight=1)
+# window.rowconfigure(1, weight=1)
 
-label1.grid(row=0, column=1, sticky='nsew')
-label2.grid(row=1, column=1, columnspan=2, sticky='nsew')
+# label1.grid(row=0, column=1, sticky='nsew')
+# label2.grid(row=1, column=1, columnspan=2, sticky='nsew')
+
+# -------------------------------------------------------------------------------------------------
+
+# place
+label1.place(x=0, y=0, width=300, height=600)                                          # valeur absolute
+label2.place(relx=0.5, rely=0, relwidth=1, relheight=1, anchor='center')               # valeur relative - 
 
 # run
 window.mainloop()
