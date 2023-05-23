8d54b70b151437d6e2bb9b265e0502694c6bcd0b
lapinRagnar <lapinragnar@gmail.com>
2023-05-23 07:38:21 +0200

    pack and padding ipadx, ipady padx, pady

---
+++ layout_pack_padding_ipad_pad.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+# window
+window = tk.Tk()
+window.geometry('400x600+500+100')
+window.title('pack in detail')
+window.bind('<Escape>', lambda event: window.quit() )
+
+# widgets
+label1 = ttk.Label(master=window, text='First label', background='#274f8f')
+label2 = ttk.Label(master=window, text='label 2', background='#a1817a')
+label3 = ttk.Label(master=window, text='last of the label', background='#07ded7')
+button = ttk.Button(window, text='button')
+
+# layout
+label1.pack(side='top', fill='both', ipady=50, padx=60)
+label2.pack(side='top', expand=True)
+label3.pack(side='top', expand=True, fill='both')
+button.pack(side='top', fill='x')
+
+
+# run
+window.mainloop()
