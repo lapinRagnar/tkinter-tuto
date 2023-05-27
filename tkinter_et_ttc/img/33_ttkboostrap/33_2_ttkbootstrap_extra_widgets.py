267cb6c3b01cf0ec838d851308a42a3421ae9402
lapinRagnar <lapinragnar@gmail.com>
2023-05-27 10:22:29 +0200

    toast notification

---
+++ tkinter_et_ttc/img/33_ttkboostrap/33_2_ttkbootstrap_extra_widgets.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.scrolled import ScrolledFrame
+from ttkbootstrap.toast import ToastNotification
+
+# window
+window = ttk.Window(themename='darkly')
+window.title('extra widgets - ttkbootstrap')
+window.geometry('800x600')
+
+# scrollable frame
+scroll_frame = ScrolledFrame(window)
+scroll_frame.pack()
+
+for i in range(100):
+  ttk.Label(scroll_frame, text=f'label : {i}').pack(fill='x')
+  ttk.Button(scroll_frame, text=f'bouton {i}').pack(fill='x')
+  
+# toast
+toast = ToastNotification(
+  title='ceci est le titre du toast', 
+  message='et ceci est le message!', 
+  duration=2000,
+  bootstyle='warning'
+  )
+  
+ttk.Button(window, text='afficher le toast', command=toast.show_toast).pack(pady=50)
+  
+# run
+window.mainloop()
