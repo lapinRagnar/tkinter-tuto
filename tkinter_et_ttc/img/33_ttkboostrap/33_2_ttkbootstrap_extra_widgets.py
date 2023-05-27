fe79a9f73d2fba62c6fe0e02ced0fc34097965ee
lapinRagnar <lapinragnar@gmail.com>
2023-05-27 10:57:04 +0200

    meter

--- tkinter_et_ttc/img/33_ttkboostrap/33_2_ttkbootstrap_extra_widgets.py
+++ tkinter_et_ttc/img/33_ttkboostrap/33_2_ttkbootstrap_extra_widgets.py
@@ -9,15 +9,15 @@ from ttkbootstrap.widgets import DateEntry, Floodgauge
 # window
 window = ttk.Window(themename='darkly')
 window.title('extra widgets - ttkbootstrap')
-window.geometry('800x600')
+window.geometry('800x800')
 
 # scrollable frame
-scroll_frame = ScrolledFrame(window)
-scroll_frame.pack()
+# scroll_frame = ScrolledFrame(window)
+# scroll_frame.pack()
 
-for i in range(100):
-  ttk.Label(scroll_frame, text=f'label : {i}').pack(fill='x')
-  ttk.Button(scroll_frame, text=f'bouton {i}').pack(fill='x')
+# for i in range(100):
+#   ttk.Label(scroll_frame, text=f'label : {i}').pack(fill='x')
+#   ttk.Button(scroll_frame, text=f'bouton {i}').pack(fill='x')
   
 # toast
 toast = ToastNotification(
@@ -50,6 +50,17 @@ progress = Floodgauge(
   )
 progress.pack(fill='x')
 ttk.Scale(window, from_=0, to=100, variable=progress_int).pack()
+
+# meter
+meter = ttk.Meter(
+    metersize=180,
+    padding=5,
+    amountused=25,
+    metertype="semi",
+    subtext="miles per hour",
+    interactive=True,
+)
+meter.pack(pady=30)
   
 # run
 window.mainloop()
