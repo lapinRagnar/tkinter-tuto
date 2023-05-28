5c5bd015c9797026c0e18f1263b7399282bb210d
lapinRagnar <lapinragnar@gmail.com>
2023-05-28 11:22:15 +0200

    refactoring folder

---
+++ tkinter_et_ttc/35_images.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import customtkinter as ctk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+# set up the window
+window = tk.Tk()
+window.title("Images")
+window.geometry("600x400")
+
+
+
+
+# run the main loop
+window.mainloop()
